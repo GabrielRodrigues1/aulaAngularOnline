0f2a26d0a26a699943b0710b936cd4282036567a
GabrielRodrigues1 <algemas123@gmail.com>
2020-05-16 03:34:28 +0000

    criando interface do somar

---
+++ primeiroApp/src/app/contar/contar.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SomarComponent } from './component/somar/somar.component';
+
+
+
+@NgModule({
+  declarations: [SomarComponent],
+  imports: [
+    CommonModule
+  ],exports:[
+    SomarComponent
+  ]
+})
+export class ContarModule { }
