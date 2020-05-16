e45cd727b9138264e0e665064a3690955b272cf0
GabrielRodrigues1 <algemas123@gmail.com>
2020-05-16 22:03:48 +0000

    menu completo

--- primeiroApp/src/app/contar/contar.module.ts
+++ primeiroApp/src/app/contar/contar.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SomarComponent } from './component/somar/somar.component';
+import { SomarService } from './services';
 
 
 
@@ -10,6 +11,10 @@ import { SomarComponent } from './component/somar/somar.component';
     CommonModule
   ],exports:[
     SomarComponent
+  ],
+  providers:[
+    SomarService
   ]
+
 })
 export class ContarModule { }
